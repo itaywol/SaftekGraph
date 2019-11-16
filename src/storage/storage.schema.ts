a2e8801cba2eab4cef6425c15dafd7e753fdc90d
Itay Wolfish <wo.itay@gmail.com>
2019-11-16 11:51:19 +0200

    moving to typescript

--- src/storage/storage.schema.ts
+++ src/storage/storage.schema.ts
@@ -1,5 +1,19 @@
 const storageModel = require('./storage.model')
 module.exports.storageTypeDefs = `
+
+    type Item { 
+        serialNumber: String
+        name: String
+        description: String
+        units: Number
+        images: [String]
+    }
+
+    type Storage {
+        location: String
+        items: [Item]
+    }
+
     extend type Query {
         helloWorld: String
     }
